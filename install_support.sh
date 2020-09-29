9e832e91cc8e2c32fd39f59d04b65a5e1ceea6d0
cesarus777 <ulius.cesarus@gmail.com>
2020-09-29 08:17:51 +0300

    Add return values for install script

--- install_support.sh
+++ install_support.sh
@@ -6,15 +6,21 @@ mkdir build
 
 cmake -B ./build -S .
 if $?
+then
   return $?
+fi
 
 cmake --build ./build
 if $?
+then
   return $?
+fi
 
 DESTDIR=./install cmake -DCMAKE_INSTALL_PREFIX=/ -P ./build/cmake_install.cmake
 if $?
+then
   return $?
+fi
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/install/support/
 
