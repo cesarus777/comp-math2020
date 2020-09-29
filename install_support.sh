9790b1b9f9dad23f529c27d146f71d77a1a53b48
cesarus777 <ulius.cesarus@gmail.com>
2020-09-29 08:39:45 +0300

    Fix install script

--- install_support.sh
+++ install_support.sh
@@ -4,23 +4,24 @@ rm -rf build install
 
 mkdir build
 
-cmake -B ./build -S .
-if $?
-then
-  return $?
-fi
+while true; do
+  cmake -B ./build -S .
+  if $?; then
+    break
+  fi
 
-cmake --build ./build
-if $?
-then
-  return $?
-fi
+  cmake --build ./build
+  if $?; then
+    break
+  fi
 
-DESTDIR=./install cmake -DCMAKE_INSTALL_PREFIX=/ -P ./build/cmake_install.cmake
-if $?
-then
-  return $?
-fi
+  DESTDIR=./install cmake -DCMAKE_INSTALL_PREFIX=/ -P ./build/cmake_install.cmake
+  if $?; then
+    break
+  fi
+
+  break
+done
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/install/support/
 
