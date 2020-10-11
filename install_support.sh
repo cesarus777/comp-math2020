888d073bf032ec8612a54a26a4d02ec78b4cca9b
cesarus777 <ulius.cesarus@gmail.com>
2020-10-11 03:51:55 +0300

    Improve install process

--- install_support.sh
+++ install_support.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-rm -rf build install
-
-mkdir build
-
-while true; do
-  cmake -B ./build -S .
-  if $?; then
-    break
-  fi
-
-  cmake --build ./build
-  if $?; then
-    break
-  fi
-
-  DESTDIR=./install cmake -DCMAKE_INSTALL_PREFIX=/ -P ./build/cmake_install.cmake
-  if $?; then
-    break
-  fi
-
-  break
-done
-
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/install/support/
+if bash -- ./support/install.sh $* && [[ ${LD_LIBRARY_PATH} != *"/install/support/"* ]]; then
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/install/support/
+fi
 
